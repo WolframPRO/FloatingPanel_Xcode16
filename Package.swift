4e676865814d302a282eed49cb97a2c71ea5e11d
Vladimir Petrov <vladimir.petrov@uula.com>
2024-06-11 15:16:34 +0400

    Package bump

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FloatingPanel",
     platforms: [
-        .iOS(.v11)
+      .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -23,5 +23,5 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "FloatingPanel", path: "Sources"),
     ],
-    swiftLanguageVersions: [.version("5")]
+    swiftLanguageVersions: [.v5]
 )
